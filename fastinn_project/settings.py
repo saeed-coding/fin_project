d17aadbc70eb13f6a956318057253a91217611fd
Saeed Anwer <saeed975.pk@gmail.com>
2025-08-14 13:10:17 +0500

    Changes

--- fastinn_project/settings.py
+++ fastinn_project/settings.py
@@ -48,6 +48,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:5500",
     "http://127.0.0.1:5173",
     "http://144.172.94.64",  # Added your server IP
 ]
