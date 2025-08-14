fdae1016d3c319a744fbf940901a316d79851292
Saeed Anwer <saeed975.pk@gmail.com>
2025-08-14 14:11:24 +0500

    Changes

--- fastinn_project/urls.py
+++ fastinn_project/urls.py
@@ -7,11 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('fin/get_data/', get_data, name="get_data"),
     path('fin/data/<int:pk>/', get_single_entry, name='get-single-entry'),
-    # path('fin/register/', RegisterView.as_view(), name='RegisterView'),
-    # path("fin/login/", LoginView.as_view(), name="login"),
     path('fin/search_data', search_data, name='search_data'),
     path('fin/upload_file', CSVUploadView.as_view(), name='upload_file'),
-    path('fin/register', register, name='register'),
+    # path('fin/register', register, name='register'),
     path('fin/login', login, name='login'),
 
 
--- users/views.py
+++ users/views.py
@@ -6,17 +6,17 @@ from django.contrib.auth.models import User
 
 
 
-@api_view(['POST'])
-def register(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
-
-    if not username or not password:
-        return Response({"error": "Mising username or password field"}, status=status.HTTP_400_BAD_REQUEST)
-    if User.objects.filter(username=username).exists():
-        return Response({"error": "username already exists"}, status=status.HTTP_400_BAD_REQUEST)
-    user = User.objects.create_user(username=username, password=password)
-    return Response({"message": "User Registered"}, status=status.HTTP_201_CREATED)
+# @api_view(['POST'])
+# def register(request):
+#     username = request.data.get('username')
+#     password = request.data.get('password')
+#
+#     if not username or not password:
+#         return Response({"error": "Mising username or password field"}, status=status.HTTP_400_BAD_REQUEST)
+#     if User.objects.filter(username=username).exists():
+#         return Response({"error": "username already exists"}, status=status.HTTP_400_BAD_REQUEST)
+#     user = User.objects.create_user(username=username, password=password)
+#     return Response({"message": "User Registered"}, status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
