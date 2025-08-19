acb31502cdbd128d740f2745d310ccea95d4fc84
Saeed Anwer <saeed975.pk@gmail.com>
2025-08-19 18:07:34 +0500

    Done

--- fastinn_project/settings.py
+++ fastinn_project/settings.py
@@ -50,6 +50,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501",
     "http://127.0.0.1:5173",
     "http://144.172.94.64",  # Added your server IP
 ]
@@ -69,8 +70,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken',
-    # 'django.contrib.auth',
-    # 'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
